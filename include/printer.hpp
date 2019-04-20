c6de29b376e6bd4f0645e491b25b98e36ad7d455
Robert Kumanek <academy.7.student.ext@nokia.com>
2019-04-20 20:54:44 +0200

    Prepare project to develop by TDD

---
+++ include/printer.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include<iostream>
+
+template<typename T>
+void printer( T x )
+{
+    std::cout << x;
+}
---
+++ uts/printerTest.cpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "gtest/gtest.h"
+#include "printer.hpp"
+
+TEST( PrinterOnStringStream, SingleArgument )
+{
+    EXPECT_TRUE(true);
+}
